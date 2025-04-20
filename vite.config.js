e3c402ff723c0edb3f46054ba4305f5606e9da2f
KeaMeng <2023484ing@aupp.edu.kh>
2025-04-20 11:08:52 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,13 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import dotenv from "dotenv";
-dotenv.config();
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-    port: parseInt(process.env.PORT, 10) || 3000, // Use the PORT environment variable or default to 3000
-    host: "0.0.0.0", // Bind to all network interfaces
-    port: process.env.PORT || 3000, // Use the PORT environment variable or default to 3000
   },
 });
